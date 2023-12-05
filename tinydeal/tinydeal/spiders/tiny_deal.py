332c7e44103d452c4a7bf33eef0907308d59acce
zakashahmurzada <109436837+zakashahmurzada@users.noreply.github.com>
2023-12-06 00:23:12 +0400

    Changed the User-Agent to hide scrapy

--- tinydeal/tinydeal/spiders/tiny_deal.py
+++ tinydeal/tinydeal/spiders/tiny_deal.py
@@ -6,6 +6,10 @@ class TinyDealSpider(scrapy.Spider):
     allowed_domains = ["web.archive.org"]
     start_urls = ['https://web.archive.org/web/20190225123327/https://www.tinydeal.com/specials.html']
     
+    def start_requests(self):
+        yield scrapy.Request(url='https://web.archive.org/web/20190225123327/https://www.tinydeal.com/specials.html', callback=self.parse, headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+        })
 
     def parse(self, response):
         for product in response.xpath('//ul[@class="productlisting-ul"]/div/li'):
@@ -14,10 +18,14 @@ class TinyDealSpider(scrapy.Spider):
                 'title': product.xpath('.//a[@class="p_box_title"]/text()').get(),
                 'url': response.urljoin(product.xpath('.//a[@class="p_box_title"]/@href').get()),
                 'discounted_price': product.xpath('.//div[@class="p_box_price"]/span[1]/text()').get(),
-                'original_price': product.xpath('.//div[@class="p_box_price"]/span[2]/text()').get()
+                'original_price': product.xpath('.//div[@class="p_box_price"]/span[2]/text()').get(),
+                'User-Agent': response.request.headers['User-Agent']
             }
 
         next_page = response.xpath('//a[@class="nextPage"]/@href').get()
 
         if next_page:   #This condition means if next page exists ?
-            yield scrapy.Request(url=next_page, callback=self.parse)
+            yield scrapy.Request(url=next_page, callback=self.parse, headers=
+                                 {
+                                     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+                                 })
